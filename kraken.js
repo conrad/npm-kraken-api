aeb6b4d86f1ecb567418e8a45fb7ca3ab8a6d681
Matt Conrad <mmconrad@gmail.com>
2017-12-20 20:36:02 -0800

    Update error handling to take string

--- kraken.js
+++ kraken.js
@@ -42,16 +42,8 @@ const rawRequest = async (url, headers, data, timeout) => {
 	const { body } = await got(url, options);
 	const response = JSON.parse(body);
 
-	if(response.error && response.error.length) {
-		const error = response.error
-			.filter((e) => e.startsWith('E'))
-			.map((e) => e.substr(1));
-
-		if(!error.length) {
-			throw new Error("Kraken API returned an unknown error");
-		}
-
-		throw new Error(error.join(', '));
+	if(response.error) {
+		throw new Error(response.error)
 	}
 
 	return response;
